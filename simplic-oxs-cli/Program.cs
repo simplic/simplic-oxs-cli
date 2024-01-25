aaa97a57643351525b758e33cc7fa8b4007e2f03
mattis.bergmann <mattis.bergmann@gmx.de>
2024-01-25 16:32:18 +0100

    Add project files.

---
+++ simplic-oxs-cli/Program.cs
@@ -0,0 +1,32 @@
+﻿using CommonServiceLocator;
+using Simplic.Studio.Ox;
+
+namespace simplic_oxs_cli
+{
+    internal class Program
+    {
+        async static Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Util.InitializeFramework();
+            Util.AddModule<Simplic.PlugIn.Studio.Ox.Server.FrameworkEntryPoint>();
+            Util.InitializeOxS();
+
+            Util.SetConnectionString("UID=admin;PWD=school;Server=sc-dev02;dbn=simplic;ASTART=No;links=tcpip");
+
+            var tenantService = ServiceLocator.Current.GetInstance<ITenantMapService>();
+            Console.WriteLine("Getting tenants");
+            var tenants = await tenantService.GetStudioMap();
+            foreach(var tenant in tenants.Keys)
+            {
+                Console.WriteLine(tenant);
+            }
+
+            Console.WriteLine("Getting services");
+            foreach (var service in Util.GetAllServices())
+            {
+                Console.WriteLine(service.ContextName);
+            }
+        }
+    }
+}
---
+++ simplic-oxs-cli/Util.cs
@@ -0,0 +1,65 @@
+﻿using CommonServiceLocator;
+using Simplic.Base;
+using Simplic.Framework.Base;
+using Simplic.Framework.DAL;
+using Simplic.Sql;
+using Simplic.Sql.SqlAnywhere.Service;
+using Simplic.Studio.Ox;
+using Unity;
+using Unity.ServiceLocation;
+
+namespace simplic_oxs_cli
+{
+    public static class Util
+    {
+        public static readonly UnityContainer container = new UnityContainer();
+
+        /// <summary>
+        /// Setup the framework
+        /// </summary>
+        public static void InitializeFramework()
+        {
+            Console.WriteLine("Initializing framework...");
+
+            container.RegisterType<ISqlService, SqlService>();
+            var locator = new UnityServiceLocator(container);
+            ServiceLocator.SetLocatorProvider(() => locator);
+
+            // Initialize framework
+            GlobalSettings.UseIni = false;
+            GlobalSettings.UserId = 0;
+            GlobalSettings.MainThread = Thread.CurrentThread;
+            GlobalSettings.UserName = "ProjectCLI";
+
+            Console.WriteLine("Initialized framework");
+        }
+
+        public static void SetConnectionString(string connection)
+        {
+            GlobalSettings.SetPrivateConnectionString(connection);
+            GlobalSettings.UserConnectionString = connection;
+            DALManager.Init(connection);
+        }
+
+        public static void AddModule<TModule>() where TModule : IFrameworkEntryPoint, new()
+        {
+            Console.WriteLine($"Registering module: {typeof(TModule).FullName}");
+
+            var module = new TModule();
+            module.Initilize();
+
+            Console.WriteLine($"Registered module");
+        }
+
+        public static void InitializeOxS()
+        {
+            Console.WriteLine("Initializing OxS");
+            Console.WriteLine("Initialized OxS");
+        }
+
+        public static IEnumerable<IInstanceDataUploadService> GetAllServices()
+        {
+            return container.ResolveAll<IInstanceDataUploadService>();
+        }
+    }
+}
